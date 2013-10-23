1cb6e5afdf541cc0548588f800fec801af32c09f
John Wright <johngeorge.wright@gmail.com>
2013-10-23 23:22:47 +0100

    Trying semantic ui

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
@@ -21,6 +21,16 @@ module.exports = function(grunt) {
     //     dest: 'dist/<%= pkg.name %>.js'
     //   }
     // },
+    copy: {
+      semanticUI: {
+        files: [{
+          expand: true,
+          cwd: 'vendor/semantic-ui/build/packaged',
+          src: '**',
+          dest: 'build/vendor/semantic-ui'
+        }]
+      }
+    },
     // uglify: {
     //   options: {
     //     banner: '<%= banner %>'
@@ -33,7 +43,7 @@ module.exports = function(grunt) {
     jade: {
       dist: {
         options: {
-          debug: false
+          pretty: false
         },
         files: [{
           expand: true,
@@ -42,6 +52,12 @@ module.exports = function(grunt) {
           dest: 'build',
           ext: '.html'
         }]
+      },
+      dev: {
+        options: {
+          pretty: true
+        },
+        files: '<%= jade.dist.files %>'
       }
     },
     jshint: {
@@ -64,13 +80,34 @@ module.exports = function(grunt) {
         src: 'Gruntfile.js'
       }
     },
+    less: {
+      dist: {
+        options: {
+          compress: true
+        },
+        files: {
+          "build/css/main.css": "source/less/**/*.less"
+        }
+      },
+      dev: {
+        options: {
+          compress: false,
+          dumpLineNumbers: true
+        },
+        files: '<%= less.dist.files %>'
+      }
+    },
     watch: {
       options: {
         atBegin: true
       },
+      less: {
+        files: '<%= less.dev.files %>',
+        tasks: ['less:dev']
+      },
       templates: {
         files: 'source/jade/**/*.jade',
-        tasks: ['jade:dist']
+        tasks: ['jade:dev']
       },
       jshint: {
         files: '<%= jshint.gruntfile.src %>',
@@ -81,13 +118,15 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   // grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'jade:dist']);
+  grunt.registerTask('default', ['jshint', 'jade:dist', 'copy:semanticUI', 'less:dist']);
 
 };
 
---
+++ README.md
@@ -0,0 +1,16 @@
+johngeorgewright.github.io
+==========================
+
+My GitHub pages.
+
+Developed using Grunt.
+
+Developing
+----------
+
+1. Install Node.js
+2. Install global scripts `sudo npm i -g grunt-cli foreman bower`
+3. Clone this repo
+4. Install npm requirements for the project `npm i`
+5. Run the site: `nf start`
+
